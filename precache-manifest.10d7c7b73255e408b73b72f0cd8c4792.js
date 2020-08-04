7bb47d5f11584225e0b3572614280c5940d1ea83
Wesley Zeng <wesleyzeng1994@gmail.com>
2020-08-04 15:07:56 -0400

    Updates

---
+++ precache-manifest.10d7c7b73255e408b73b72f0cd8c4792.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b35f5806e829535b55d8d04572a6e982",
+    "url": "/Recipe/React-Deploy/index.html"
+  },
+  {
+    "revision": "44544e11611e2949847f",
+    "url": "/Recipe/React-Deploy/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "9632ae4f0425aee85c38",
+    "url": "/Recipe/React-Deploy/static/css/main.b4f8ac63.chunk.css"
+  },
+  {
+    "revision": "44544e11611e2949847f",
+    "url": "/Recipe/React-Deploy/static/js/2.f209ae67.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Recipe/React-Deploy/static/js/2.f209ae67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9632ae4f0425aee85c38",
+    "url": "/Recipe/React-Deploy/static/js/main.686eebab.chunk.js"
+  },
+  {
+    "revision": "f29666fa27f2a6c52517",
+    "url": "/Recipe/React-Deploy/static/js/runtime-main.0206d19f.js"
+  }
+]);
